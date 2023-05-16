061131f6076a5b5a5de6ac670a5d5355d5d85ffe
IgorLekuona <ilekuona.work@gmail.com>
2023-05-16 20:26:02 +0200

    actividad 1

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -13,4 +13,24 @@ function ejercicio1 () {
     alert(frase);
 }
 
-export {ejercicio1};
+function ejercicio4 () {
+    let res = [];
+    let ind = 7;
+    while (ind <= 100) {
+        res.push(ind);
+        ind += 7;
+    }
+    console.log(res.toString());
+}
+
+const formatter = {
+    prefix: "Hello",
+    append: function(arg) {
+        let concat = this.prefix + " " + arg;
+        console.log(concat);
+    },
+    toLowerString: function(arg) {
+        console.log(String(arg).toLowerCase());
+    }
+}
+export { ejercicio1, ejercicio4, formatter };
--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,5 +1,5 @@
+import { ejercicio1, ejercicio4, formatter } from "./aboutme.js";
 // Ejercicio 1
-import "./aboutme.js";
 ejercicio1();
 console.log("Ejercicio 1");
 
@@ -8,8 +8,13 @@ console.log("Ejercicio 1");
 // Ejercicio 3
 
 // Ejercicio 4
-
+ejercicio4();
+console.log("Ejercicio 4");
 // Ejercicio 5
 
 // Ejercicio 6
+formatter.append("Igor");
+console.log("Ejercicio 6 - metodo append");
+formatter.toLowerString("Hello I'm Igor!");
+console.log("Ejercicio 6 - metodo toLowerString");
 
