7da4c000ee688935ca4691a53c8cd0183b203e0f
IgorLekuona <ilekuona.work@gmail.com>
2023-05-29 16:42:18 +0200

    actividad 3

--- activity/typescript/index.js
+++ activity/typescript/index.js
@@ -1,4 +1,61 @@
-// Ejercicio 1
+;
+var leerPerson = function (pers) { return [pers.name, Number(pers.age).toString(), pers.profession]; };
+var newperson = {
+    name: "Igor",
+    age: 25,
+    profession: "Frontend developer"
+};
+console.log("[" + leerPerson(newperson).toString() + "]");
 // Ejercicio 2
-// Ejercicio 3
+function sumOrConcatenate(v1, v2) {
+    if (typeof v1 === "number" && typeof v2 === "number")
+        return v1 + v2;
+    else if (typeof v1 === "string" && typeof v2 === "string")
+        return v1.concat("-").concat(v2);
+    else
+        return (typeof v1 === "string" ? v1.concat(":").concat(Number(v2).toString()) : Number(v1).toString().concat(":").concat(String(v2)));
+}
+;
+console.log(sumOrConcatenate(4, 3));
+console.log(sumOrConcatenate(4, "hello"));
+console.log(sumOrConcatenate("hello", "world"));
+;
+;
+function isCar(mach) {
+    return mach.pressPedal !== undefined;
+}
+;
+function startMachine(mach) {
+    mach.turnOnEngine();
+    isCar(mach) ? mach.pressPedal() : mach.openThrottle();
+}
+;
+var carObj = {
+    tires: 4,
+    turnOnEngine: function () { console.log("Turning car on"); },
+    pressPedal: function () { console.log("Pressing pedal"); }
+};
+var motorcycleObj = {
+    tires: 3,
+    turnOnEngine: function () { console.log("Turning motorcycle on"); },
+    openThrottle: function () { console.log("Opening the throttle"); }
+};
+startMachine(carObj);
+startMachine(motorcycleObj);
 // Ejercicio 4
+function removeFirstEntry(arr) {
+    arr.shift();
+    return arr;
+}
+;
+var strArray = ['Hello', 'World', 'Im', 'a', 'Full', 'Stack', 'Developer'];
+var numArray = [1, 2, 3, 4, 5, 6, 7];
+var mixedArray = ['Hello', 'I', 'have', 3, 'tasks'];
+var unsupportedArray = [{ name: 'Lucas', surname: 'Fernandez' }, 'Hello', 22];
+var newStrArray = removeFirstEntry(strArray);
+var newNumArray = removeFirstEntry(numArray);
+var newMixedArray = removeFirstEntry(mixedArray);
+// const newUnsupportedArray = removeFirstEntry(unsupportedArray); --> will fail
+console.log(newStrArray); // --> ['World', 'Im', 'a', 'Full', 'Stack', 'Developer'];
+console.log(newNumArray); // --> [2, 3, 4, 5, 6, 7];
+console.log(newMixedArray); // --> ['I', 'have', 3, 'tasks'];
--- activity/typescript/index.ts
+++ activity/typescript/index.ts
@@ -1,7 +1,83 @@
 // Ejercicio 1
+interface Person {
+    name: string,
+    age: number,
+    profession: string
+};
+
+let leerPerson = (pers: Person): string[] => [pers.name, Number(pers.age).toString(), pers.profession];
+
+let newperson: Person = {
+    name: "Igor",
+    age: 25,
+    profession: "Frontend developer"
+};
+console.log("["+ leerPerson(newperson).toString() +"]");
 
 // Ejercicio 2
+function sumOrConcatenate(v1, v2: number | string): number | string {
+    if (typeof v1 === "number" && typeof v2 === "number") return v1+v2;
+    else if (typeof v1 === "string" && typeof v2 === "string") return v1.concat("-").concat(v2);
+    else return (typeof v1 === "string" ? v1.concat(":").concat(Number(v2).toString()) : Number(v1).toString().concat(":").concat(String(v2)));
+};
+
+console.log(sumOrConcatenate(4,3));
+console.log(sumOrConcatenate(4,"hello"));
+console.log(sumOrConcatenate("hello","world"));
 
 // Ejercicio 3
+interface Car {
+    tires: number,
+    turnOnEngine: () => void,
+    pressPedal: () => void
+};
+
+interface Motorcycle {
+    tires: number,
+    turnOnEngine: () => void,
+    openThrottle: () => void
+};
+
+function isCar(mach: Car | Motorcycle): mach is Car {
+    return (mach as Car).pressPedal !== undefined;
+};
+
+function startMachine(mach: Car | Motorcycle) {
+    mach.turnOnEngine();
+    isCar(mach) ? mach.pressPedal() : mach.openThrottle(); 
+};
+
+let carObj: Car = {
+    tires: 4,
+    turnOnEngine: function () {console.log("Turning car on");},
+    pressPedal: function () {console.log("Pressing pedal");}
+}
+
+let motorcycleObj: Motorcycle = {
+    tires: 3,
+    turnOnEngine: function () {console.log("Turning motorcycle on");},
+    openThrottle: function () {console.log("Opening the throttle");}
+}
+
+startMachine(carObj);
+startMachine(motorcycleObj);
 
 // Ejercicio 4
+function removeFirstEntry(arr: Array<string | number>): Array<string | number> {
+    arr.shift();
+    return arr;
+};
+
+const strArray: string[] = ['Hello', 'World', 'Im', 'a', 'Full', 'Stack', 'Developer'];
+const numArray: number[] = [1, 2, 3, 4, 5, 6, 7];
+const mixedArray: Array<number|string> = ['Hello', 'I', 'have', 3, 'tasks'];
+const unsupportedArray = [{name: 'Lucas', surname: 'Fernandez'}, 'Hello', 22];
+
+const newStrArray = removeFirstEntry(strArray);
+const newNumArray = removeFirstEntry(numArray);
+const newMixedArray = removeFirstEntry(mixedArray);
+// const newUnsupportedArray = removeFirstEntry(unsupportedArray); --> will fail
+
+console.log(newStrArray); // --> ['World', 'Im', 'a', 'Full', 'Stack', 'Developer'];
+console.log(newNumArray); // --> [2, 3, 4, 5, 6, 7];
+console.log(newMixedArray); // --> ['I', 'have', 3, 'tasks'];
